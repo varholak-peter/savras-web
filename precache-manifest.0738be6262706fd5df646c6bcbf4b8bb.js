e48d521ac75fd4c89d740daf32f0e80317ee4f67
varholak-peter <peter.varholak@gmail.com>
2020-05-30 16:16:51 +0200

    Updates

---
+++ precache-manifest.0738be6262706fd5df646c6bcbf4b8bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86d8df44fc0c5882220971b3552da4b1",
+    "url": "/savras-web/index.html"
+  },
+  {
+    "revision": "fc685eb14df304a20a2e",
+    "url": "/savras-web/static/css/2.bb342caa.chunk.css"
+  },
+  {
+    "revision": "fc685eb14df304a20a2e",
+    "url": "/savras-web/static/js/2.7d9ef8ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/savras-web/static/js/2.7d9ef8ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9e4b322c948db79112f",
+    "url": "/savras-web/static/js/main.56a53d5a.chunk.js"
+  },
+  {
+    "revision": "3e4bf4ca07d9c903179a",
+    "url": "/savras-web/static/js/runtime-main.2524472a.js"
+  }
+]);
